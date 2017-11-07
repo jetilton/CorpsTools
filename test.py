ad1cc5f62422431af14ba77413532d4f93da26fe
jetilton <jfftilton@gmail.com>
2017-11-07 12:42:16 -0800

    fixes to merge and get_ts tests

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 import unittest
 from CorpsTools import CWMSHydro
 import CorpsTools
-import TimeSeriesObj
+from TimeSeriesObj import TimeSeriesObj
 import pandas 
 
 c = CWMSHydro(7)
@@ -35,10 +35,16 @@ class CWMSHydroMethods(unittest.TestCase):
         
     def test_get_ts(self):
         path = 'GCL.Elev-Forebay.Ave.~1Day.1Day.CBT-REV'
+        path_list = ['TDDO.Temp-Water.Inst.1Hour.0.GOES-REV','TDDO.Pres-Water-TotalGas.Inst.1Hour.0.GOES-REV']
         ts = c.get_ts(path, merge = False)
         c.get_ts(path, merge = True)
-        self.assertTrue(isinstance(ts, TimeSeriesObj.TimeSeriesObj))
-        self.assertTrue(bool(c.ts['daily']['column_data']==ts['daily']['column_data']))
+        self.assertTrue(isinstance(ts, TimeSeriesObj))
+        self.assertTrue(isinstance(c.ts['daily'], pandas.core.frame.DataFrame))
+        ts = c.get_ts(path_list)
+        c.get_ts(path_list, merge = True)
+        self.assertTrue(isinstance(c.ts['hourly'], pandas.core.frame.DataFrame))
+        self.assertTrue(len(c.ts['hourly'].columns)==2)
+        
         
     def test_merge(self):
         """
@@ -50,9 +56,9 @@ class CWMSHydroMethods(unittest.TestCase):
         ts2 = c.get_ts(path2)
         c.merge(ts)
         c.merge(ts2)
-        df_hourly = c.ts['hourly']['df']
-        df_daily = c.ts['daily']['df']
-        self.assertTrue(isinstance(c.ts, TimeSeriesObj.TimeSeriesObj))
+        df_hourly = c.ts['hourly']
+        df_daily = c.ts['daily']
+        self.assertTrue(isinstance(c.ts, TimeSeriesObj))
         self.assertTrue(len(c.ts.keys()) ==2)
         self.assertTrue(isinstance(df_daily,pandas.core.frame.DataFrame))
         self.assertTrue(isinstance(df_hourly,pandas.core.frame.DataFrame))
