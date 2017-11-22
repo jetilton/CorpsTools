26dc36a5867a9a999cc61a2023b84f0dc502cd6d
jetilton <jfftilton@gmail.com>
2017-11-22 13:34:24 -0800

    started ets func

--- smooth.py
+++ smooth.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
-from rpy2.robjects import r
+import pandas as pdfrom 
 
-
+from rpy2.robjects import r, pandas2ri
+import numpy as np
+from rpy2.robjects.packages import importr
 
 def rmse(y, yhat):
     n = len(y)
@@ -23,12 +24,13 @@ def ses_series(series, alpha, error = False):
     yhat = pd.Series(yhat_list)
     return yhat
 
-def decompose(series, frequency, s_window, **kwargs):
+def decompose(series, frequency, s_window, log = False, **kwargs):
     '''Use STL to decompose the time series into seasonal, trend, and
     residual components.'''
         
     df = pd.DataFrame()
     df['date'] = series.index
+    if log: series = series.pipe(np.log)
     s = [x for x in series.values]
     length = len(series)
     s = r.ts(s, frequency=frequency)
@@ -41,3 +43,32 @@ def decompose(series, frequency, s_window, **kwargs):
         
         
 
+def ets(series):
+    '''Use STL to decompose the time series into seasonal, trend, and
+    residual components.'''
+    forecast = importr('forecast')
+    s = pandas2ri.py2ri(series)
+    s = r.ts(s, frequency=frequency)
+    s = forecast.ets(s)
+    names = [x for x in r.names(s)]
+    s.rx()
+    for name in names:
+        print(name,len([x for x in s.rx(name)[0]]))
+    forecast = importr('forecast')    
+    df = pd.DataFrame()
+    df['date'] = series.index
+    
+    s = [x for x in series.values]
+    length = len(series)
+    s = r.ts(series, frequency=frequency)
+    s = forecast.ets(s)
+    decomposed = [x for x in r.stl(s, s_window).rx2('time.series')]
+    df['observed'] = series.values
+    df['trend'] = decomposed[length:2*length]
+    df['seasonal'] = decomposed[0:length]
+    df['residuals'] = decomposed[2*length:3*length]
+    return df
+
+
+
+series = 
