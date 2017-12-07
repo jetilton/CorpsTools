3f5fccf17324934ae216d16cdaded389b84f8ee2
jetilton <jfftilton@gmail.com>
2017-12-07 07:56:52 -0800

    deleted utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def check_obj(obj, obj_type):
-    try:
-        raise TypeError(obj)
-    except TypeError as e:  # as e syntax added in ~python2.5
-        if not isinstance(obj, obj_type):
-            raise
